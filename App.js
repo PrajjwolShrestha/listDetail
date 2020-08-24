28356a653797fb3aa00d4b304fb95fbd456effdc
Prajjwol <prajjwolshrestha@icloud.com>
2020-08-24 18:06:08 +1000

    first commit

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+
+//1. install navigation library by typing in terminal ----  npm install @react-navigation/native --save
+//2. type this comman in terminal --  expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+//3. npm install @react-navigation/stack
+
+
+import { HomeScreen} from './components/HomeScreen'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+
+//initialize a variable
+const Stack = createStackNavigator()
 
 const styles = StyleSheet.create({
   container: {
